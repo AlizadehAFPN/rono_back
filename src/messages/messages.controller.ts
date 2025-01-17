159b90873a0b4b5e091834ec78037360b2011ad3
AlizadehAFPN <alizadeh2023@gmail.com>
2025-01-17 20:01:16 +0330

    DI

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { createMessageDto } from './dtos/create.message.dto';
+import { MessagesServices } from './messages.services';
 
 @Controller('messages')
 export class MessagesController {
+    constructor (public messagesServices : MessagesServices ) {}
+
     @Get()
     listMessages() {
+        return this.messagesServices.findAll()
     }
 
     @Post()
     createMessages(@Body() body : createMessageDto)  {
-        console.log(body , 'body')
+        return this.messagesServices.create(body?.content)
     }
     @Get("/:id")
-    oetMessage (@Param ('id') id: number) {
-        console.log(id , 'id')
+    async oetMessage (@Param ('id') id: string) {
+        const message = await this.messagesServices.findOne(id)
+
+        if(!message){
+            throw new NotFoundException('Message Not Found')
+        }
+        return message
     }
 }
